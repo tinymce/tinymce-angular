97b5e1a51c64aae9637d40e92b77996f75d0bc18
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-01-03 17:23:52 +0100

    added utils

--- src/app/modules/editor/editor.component.ts
+++ src/app/modules/editor/editor.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as ScriptLoader from '../../../utils/ScriptLoader';
+import {} from '../../../utils/Utils';
+import { getTinymce } from '../../../TinyMCE';
+
+const scriptState = ScriptLoader.create();
+
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'editor',
