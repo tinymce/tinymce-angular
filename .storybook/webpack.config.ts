27baf187eebabdbe27970074c25b573c2867b107
Simon Fjeldså <simon.fjeldsa@tiny.cloud>
2019-05-30 13:40:49 +0200

    TINY-3740: Fix breaking change after upgrading storybook

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -1,9 +1,13 @@
 const path = require('path');
 
-module.exports = (baseConfig: any) => {
-  baseConfig.module.rules.push({
+module.exports = async ({ config }: any) => {
+  // Remove Storybook's default loaders for css
+  config.module.rules = config.module.rules.filter((rule: any) => rule.test.toString() !== '/\\.css$/');
+
+  config.module.rules.push({
     test: /\.css$/,
     use: ['to-string-loader', 'css-loader']
   });
-  return baseConfig;
+
+  return config;
 };
