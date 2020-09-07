820d591a94fe54b30c0a8b724bce83cc3ea9a4e3
Simon Fjeldsa <simon.fjeldsa@tiny.cloud>
2020-09-07 16:37:18 +0200

    Upgrade storybook

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure, addDecorator } from '@storybook/angular';
-import { withNotes } from '@storybook/addon-notes';
-
-addDecorator(withNotes);
-function loadStories() {
-  require('../stories/index.ts');
-}
-
-configure(loadStories, module);
