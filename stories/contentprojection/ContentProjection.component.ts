9983589ce89f328cc00f3f08a6e7c9d39453a1f2
Simon Fjeldsa <simon.fjeldsa@tiny.cloud>
2020-09-08 16:01:35 +0200

    Simplify transclusion example

---
+++ stories/contentprojection/ContentProjection.component.ts
@@ -0,0 +1,59 @@
+import { Component, TemplateRef, Input } from '@angular/core';
+import { apiKey } from '../Settings';
+// tslint:disable:max-classes-per-file
+
+/*
+  Normally when projecting content you would use ng-content but this is not something that works
+  with TinyMCE due to its usage of an iframe (when in iframe mode). The reason for this is that
+  the lifecycle of ng-content is controlled by its parent view and not where it's consumed. In
+  other words, the projected content can be initialized and destroyed when not actually being in
+  the DOM. Iframes will re-render whenever they are detached/attached to the DOM and thus this
+  breaks TinyMCE.
+
+  A workaround is to use a template outlet instead of content projection. The result is what you
+  would expect from content projection, but with the lifecycle being in sync to where the content
+  is being consumed.
+*/
+
+@Component({
+  selector: 'container',
+  styles: [`
+    .container {
+      border: 1px solid blue;
+      display: block;
+      padding: 15px;
+    }
+  `],
+  template: `
+    <ng-template #placeHolder>
+      <p>I am a placeholder.</p>
+    </ng-template>
+
+    <div [ngClass]="'container'">
+      <button [innerText]="show ? 'Hide' : 'Show'" (click)="handleToggle()"></button>
+      <ng-container *ngTemplateOutlet="show ? editorTemplate : placeHolder"></ng-container>
+    <div>
+  `
+})
+export class ContainerComponent {
+  public show = true;
+  @Input() editorTemplate!: TemplateRef<any>;
+
+  public handleToggle() {
+    this.show = !this.show;
+  }
+}
+
+@Component({
+  template: `
+    <ng-template #editorTemplate>
+      <editor [apiKey]="apiKey" [(ngModel)]="editorValue"></editor>
+    </ng-template>
+
+    <container [editorTemplate]="editorTemplate"></container>
+  `
+})
+export class ContentProjectionComponent {
+  public apiKey = apiKey;
+  public editorValue = '';
+}
