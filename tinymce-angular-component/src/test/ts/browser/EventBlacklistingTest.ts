96aca7d98dea538a7dba0deec66e54957f1406e3
Seb <jscasca@gmail.com>
2020-09-10 12:05:34 +1000

    INT-2238: Update tests

--- tinymce-angular-component/src/test/ts/browser/EventBlacklistingTest.ts
+++ tinymce-angular-component/src/test/ts/browser/EventBlacklistingTest.ts
@@ -1,63 +1,66 @@
 import '../alien/InitTestEnvironment';
 
 import { NgZone } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Assertions, Chain, Pipeline, Log } from '@ephox/agar';
+import { TestBed } from '@angular/core/testing';
+import { Chain, Pipeline, Log, Waiter } from '@ephox/agar';
 import { UnitTest } from '@ephox/bedrock-client';
 import { VersionLoader } from '@tinymce/miniature';
 
 import { EditorComponent } from '../../../main/ts/public_api';
+import { TestStore } from '../alien/TestStore';
 
 UnitTest.asynctest('EventBlacklistingTest', (success, failure) => {
 
-  const createEditor = (): ComponentFixture<EditorComponent> => {
+  const store = TestStore();
+
+  const cSetupEnv = () => Chain.async((_, next, die) => {
     TestBed.configureTestingModule({
       declarations: [EditorComponent]
-    }).compileComponents();
-    const fixture = TestBed.createComponent(EditorComponent);
-    fixture.componentInstance.allowedEvents = 'onKeyUp,onClick';
-    fixture.componentInstance.ignoreEvents = 'onClick';
-    return fixture;
-  };
+    }).compileComponents().then(next, die);
+  });
 
-  const cTeardown = Chain.op(() => {
+  const cTeardownEnv = Chain.op(() => {
     TestBed.resetTestingModule();
   });
 
+  const cCreateEditor = Chain.injectThunked(() => {
+    const fixture = TestBed.createComponent(EditorComponent);
+    fixture.componentInstance.allowedEvents = 'onKeyUp,onClick';
+    fixture.componentInstance.ignoreEvents = 'onClick';
+    fixture.detectChanges();
+    return fixture;
+  });
+
+  const cTest = Chain.fromParent(cCreateEditor, [
+    Chain.op((fixture) => {
+      fixture.componentInstance.onKeyUp.subscribe(() => {
+        const inZone = NgZone.isInAngularZone();
+        store.adder('keyup.zone=' + inZone)();
+      });
+      fixture.componentInstance.onKeyDown.subscribe(store.adder('keydown'));
+      fixture.componentInstance.onClick.subscribe(store.adder('click'));
+    }),
+    Chain.op((fixture) => {
+      fixture.componentInstance.editor.fire('keydown');
+      fixture.componentInstance.editor.fire('keyclick');
+      fixture.componentInstance.editor.fire('keyup');
+    }),
+    Waiter.cTryUntil(
+      'waiting for events firing',
+      store.cAssertEq('Only keyup should fire. Other events must be ignored or not allowed', ['keyup.zone=true']),
+      1000
+    )
+  ]);
+
   const sTestVersion = (version: '4' | '5') => VersionLoader.sWithVersion(
     version,
-    Log.chainsAsStep('', 'Events should be bound when allowed', [
-      Chain.async<void, ComponentFixture<EditorComponent>>((_, next) => {
-        const fixture = createEditor();
-        fixture.detectChanges();
-        next(fixture);
-      }),
-      Chain.async<ComponentFixture<EditorComponent>, ComponentFixture<EditorComponent>>((fixture, done) => {
-        fixture.componentInstance.onKeyUp.subscribe(() => {
-          Assertions.assertEq('Subscribers to onKeyUp should run within NgZone', true, NgZone.isInAngularZone());
-          done(fixture);
-        });
-        fixture.componentInstance.editor.fire('keyup');
-      }),
-
-      Chain.async<ComponentFixture<EditorComponent>, ComponentFixture<EditorComponent>>((fixture, done) => {
-        let keyDownFlag = false;
-        fixture.componentInstance.onKeyDown.subscribe(() => {keyDownFlag = true;});
-        fixture.componentInstance.editor.fire('keydown');
-        Assertions.assertEq('Keydown is not allowed.', false, keyDownFlag);
-        done(fixture);
-      }),
-
-      Chain.async<ComponentFixture<EditorComponent>, ComponentFixture<EditorComponent>>((fixture, done) => {
-        let clickFlag = false;
-        fixture.componentInstance.onClick.subscribe(() => clickFlag = true);
-        fixture.componentInstance.editor.fire('click');
-        Assertions.assertEq('Click must be ignored.', false, clickFlag);
-        done(fixture);
-      }),
-
-      cTeardown
-    ])
+    Log.chainsAsStep('', 'Events should be bound when allowed',
+      [
+        store.cClear,
+        cSetupEnv(),
+        cTest,
+        cTeardownEnv
+      ])
   );
 
   Pipeline.async({}, [
