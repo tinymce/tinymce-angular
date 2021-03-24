01a189179178cb6ee4f6869e89679f2447557300
jscasca <jscasca@gmail.com>
2021-03-24 15:28:28 +1000

    Linting

--- tinymce-angular-component/src/main/ts/editor/editor.component.ts
+++ tinymce-angular-component/src/main/ts/editor/editor.component.ts
@@ -127,6 +127,7 @@ export class EditorComponent extends Events implements AfterViewInit, ControlVal
     this._element = document.createElement(this.inline ? tagName : 'textarea');
     if (this._element) {
       if (document.getElementById(this.id)) {
+        /* eslint no-console: ["error", { allow: ["warn"] }] */
         console.warn(`TinyMCE-Angular: an element with id [${this.id}] already exists. Editors with duplicate Id will not be able to mount`);
       }
       this._element.id = this.id;
