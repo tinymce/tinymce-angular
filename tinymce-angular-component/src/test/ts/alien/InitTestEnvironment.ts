1b6bf0a839b676abefe40a71eedd4fd04ce2b187
Daniel Oakman <141111365+danoaky-tiny@users.noreply.github.com>
2024-03-26 16:08:19 +1100

    INT-3274: Remove zone.js-11 dependency

--- tinymce-angular-component/src/test/ts/alien/InitTestEnvironment.ts
+++ tinymce-angular-component/src/test/ts/alien/InitTestEnvironment.ts
@@ -1,8 +1,6 @@
 import 'core-js/features/reflect';
-import 'zone.js-11';
-// Angular 17 uses 0.14.x of Zone.js, but that version no longer exports these two modules. So this is a workaround for now.
-import 'zone.js-11/dist/fake-async-test.js';
-import 'zone.js-11/dist/zone';
+import 'zone.js';
+import 'zone.js/plugins/fake-async-test';
 
 import { TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
