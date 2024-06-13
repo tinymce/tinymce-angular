eb1c96c2fa875535a792cc4ba171bc0d32cf70f4
Daniel Oakman <141111365+danoaky-tiny@users.noreply.github.com>
2024-06-13 11:58:56 +1000

    INT-3298: Added TestHelpers.ts

---
+++ tinymce-angular-component/src/test/ts/alien/TestHelpers.ts
@@ -0,0 +1,35 @@
+import { Fun, Global, Arr, Strings } from '@ephox/katamari';
+import { Observable, throwError, timeout } from 'rxjs';
+import { ScriptLoader } from '../../../main/ts/utils/ScriptLoader';
+import { Attribute, Remove, SelectorFilter, SugarElement } from '@ephox/sugar';
+
+export const apiKey = Fun.constant(
+  'qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc',
+);
+
+export const throwTimeout =
+  (timeoutMs: number, message: string) =>
+    <T>(source: Observable<T>) =>
+      source.pipe(
+        timeout({
+          first: timeoutMs,
+          with: () => throwError(() => new Error(message))
+        }),
+      );
+
+export const deleteTinymce = () => {
+  ScriptLoader.reinitialize();
+
+  delete Global.tinymce;
+  delete Global.tinyMCE;
+
+  const hasTinyUri = (attrName: string) => (elm: SugarElement<Element>) => Attribute.getOpt(elm, attrName).exists((src) => Strings.contains(src, 'tinymce'));
+
+  const elements = Arr.flatten([
+    Arr.filter(SelectorFilter.all('script'), hasTinyUri('src')),
+    Arr.filter(SelectorFilter.all('link'), hasTinyUri('href')),
+  ]);
+
+  Arr.each(elements, Remove.remove);
+};
+
