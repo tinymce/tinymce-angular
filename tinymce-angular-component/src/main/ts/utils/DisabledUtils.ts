f181d5e4e09ed70d4b80916d906128f568ad324f
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-05-13 15:42:54 +0200

    TINY-11907: remove dependency

--- tinymce-angular-component/src/main/ts/utils/DisabledUtils.ts
+++ tinymce-angular-component/src/main/ts/utils/DisabledUtils.ts
@@ -1,10 +1,10 @@
-import { TinyVer } from '@tinymce/miniature';
 import { getTinymce } from '../TinyMCE';
 import { TinyMCE } from 'tinymce';
 
 const isDisabledOptionSupported = () => {
   const tiny: TinyMCE = getTinymce();
-  return !TinyVer.isLessThan(tiny, '7.6.0');
+  // Disabled option is supported since Tiny 7.6.0
+  return Number(tiny.majorVersion) >= 7 && Number(tiny.minorVersion) >= 6;
 };
 
 export {
