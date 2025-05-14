d9a0f5b173b905f76d77c552a638618807f46f97
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-05-14 09:07:02 +0200

    TINY-11907: apply codereview comment

--- tinymce-angular-component/src/main/ts/utils/DisabledUtils.ts
+++ tinymce-angular-component/src/main/ts/utils/DisabledUtils.ts
@@ -4,7 +4,7 @@ import { TinyMCE } from 'tinymce';
 const isDisabledOptionSupported = () => {
   const tiny: TinyMCE = getTinymce();
   // Disabled option is supported since Tiny 7.6.0
-  return Number(tiny.majorVersion) >= 7 && Number(tiny.minorVersion) >= 6;
+  return Number(tiny.majorVersion) > 7 || (Number(tiny.majorVersion) === 7 && Number(tiny.minorVersion) >= 6);
 };
 
 export {
