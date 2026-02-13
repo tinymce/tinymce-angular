934797c04f82e486ce309140cd278ed6cfef22d0
Ben Tran <ben.tran@tiny.cloud>
2026-02-13 13:30:51 +1030

    Add smoketest file

---
+++ tinymce-angular-component/src/test/ts/smoke_test/VerifyIntegrationTest.ts
@@ -0,0 +1,54 @@
+import '../alien/InitTestEnvironment';
+
+import { Assertions } from '@ephox/agar';
+import { describe, it, context, before } from '@ephox/bedrock-client';
+import { Global } from '@ephox/katamari';
+import { Traverse, SugarElement, Attribute } from '@ephox/sugar';
+
+import { EditorComponent, TINYMCE_SCRIPT_SRC } from '../../../main/ts/public_api';
+import { Version } from '../../../main/ts/editor/editor.component';
+import { editorHook } from '../alien/TestHooks';
+import type { Editor } from 'tinymce';
+import { deleteTinymce, supportedTinymceVersions } from '../alien/TestHelpers';
+
+/*
+  This test requires the targeted Angular version provided via custom route
+*/
+describe('VerifyIntegrationTest', () => {
+  interface IntegrationInfo {
+    version: string;
+  }
+
+  const assertTinymceVersion = (version: Version, editor: Editor) => {
+    Assertions.assertEq(`Loaded version of TinyMCE should be ${version}`, version, editor.editorManager.majorVersion);
+    Assertions.assertEq(`Loaded version of TinyMCE should be ${version}`, version, Global.tinymce.majorVersion);
+  };
+  
+  for (const version of supportedTinymceVersions()) {
+    context(`With local version ${version}`, () => {
+      const createFixture = editorHook(EditorComponent, {
+        providers: [
+          {
+            provide: TINYMCE_SCRIPT_SRC,
+            useValue: `/project/node_modules/tinymce-${version}/tinymce.min.js`,
+          },
+        ],
+      });
+
+      before(deleteTinymce);
+
+      it('Should be able to load with the specified Angular version', async () => {
+        const { editor } = await createFixture();
+        const integrationInfo = await window.fetch('/custom/integration/info').then((resp) => resp.json()) as IntegrationInfo;
+        const rootEl = editor.getContainer().parentNode as Element;
+
+        Assertions.assertEq(`Angular version should be ${integrationInfo.version}`, 
+          true,
+          Attribute.get(SugarElement.fromDom(rootEl), 'ng-version') === integrationInfo.version
+        );
+
+        assertTinymceVersion(version, editor);
+      });
+    });
+  }
+});
