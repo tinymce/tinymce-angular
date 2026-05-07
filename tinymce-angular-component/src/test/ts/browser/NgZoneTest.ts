884e1bbf5526829d6c751a7cd88890fb47d2c545
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2026-05-07 13:16:09 +0200

    INT-3396: simplify ng zone tests

--- tinymce-angular-component/src/test/ts/browser/NgZoneTest.ts
+++ tinymce-angular-component/src/test/ts/browser/NgZoneTest.ts
@@ -2,28 +2,21 @@ import '../alien/InitTestEnvironment';
 
 import { NgZone, provideZoneChangeDetection } from '@angular/core';
 import { Assertions } from '@ephox/agar';
-import { beforeEach, describe, it } from '@ephox/bedrock-client';
+import { describe, it } from '@ephox/bedrock-client';
 
 import { EditorComponent } from '../../../main/ts/editor/editor.component';
-import { eachVersionContext } from '../alien/TestHooks';
+import { eachVersionContext, fixtureHook } from '../alien/TestHooks';
 import { first } from 'rxjs';
 import { throwTimeout } from '../alien/TestHelpers';
-import { TestBed } from '@angular/core/testing';
 
 describe('NgZoneTest', () => {
   eachVersionContext([ '4', '5', '6', '7', '8' ], () => {
-    beforeEach(async () => {
-      await TestBed.configureTestingModule({
-        imports: [ EditorComponent ],
-        providers: [ provideZoneChangeDetection() ]
-      }).compileComponents();
-    });
+    const createFixture = fixtureHook(EditorComponent, { imports: [ EditorComponent ], providers: [ provideZoneChangeDetection() ] });
 
     it('Subscribers to events should run within NgZone', async () => {
-      const fixture = TestBed.createComponent(EditorComponent);
+      const fixture = createFixture();
       const editor = fixture.componentInstance;
-      fixture.detectChanges(); // TODO: consider using fixture.whenStable()
-
+      fixture.detectChanges();
       await new Promise<void>((resolve) => {
         editor.onInit.pipe(first(), throwTimeout(10000, 'Timed out waiting for init event')).subscribe(() => {
           Assertions.assertEq('Subscribers to onInit should run within NgZone', true, NgZone.isInAngularZone());
@@ -34,10 +27,9 @@ describe('NgZoneTest', () => {
 
     // Lets just test one EventEmitter, if one works all should work
     it('Subscribers to onKeyUp should run within NgZone', async () => {
-      const fixture = TestBed.createComponent(EditorComponent);
+      const fixture = createFixture();
       const editor = fixture.componentInstance;
-      fixture.detectChanges(); // TODO: consider using fixture.whenStable()
-
+      fixture.detectChanges();
       await new Promise<void>((resolve) => {
         editor.onKeyUp.pipe(first(), throwTimeout(10000, 'Timed out waiting for key up event')).subscribe(() => {
           Assertions.assertEq('Subscribers to onKeyUp should run within NgZone', true, NgZone.isInAngularZone());
